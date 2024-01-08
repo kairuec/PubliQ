b92f175f7f49c0e86b769631465b5f44e06d6251
kairuec <sakurai1991@gmail.com>
2024-01-08 09:02:01 +0900

    scrfエラー検証

--- frontend/nextjs/src/lib/axios.ts
+++ frontend/nextjs/src/lib/axios.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
 const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const instance = axios.create({
   withCredentials: true,
-  withXSRFToken: true, // これを追加
+  // withXSRFToken: true, // これを追加
   headers: {
-    "X-Requested-With": "XMLHttpRequest",
+    'X-Requested-With': 'XMLHttpRequest',
   },
   baseURL: baseURL,
 });
 
-export const csrf = () => instance.get(baseURL + "/sanctum/csrf-cookie");
+export const csrf = () => instance.get(baseURL + '/sanctum/csrf-cookie');
 
 export default instance;
