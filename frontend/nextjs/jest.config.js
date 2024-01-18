d74579134f7edb2914a7edbefe7129a7af4b957c
kairuec <sakurai1991@gmail.com>
2024-01-18 13:03:44 +0900

    jest導入

---
+++ frontend/nextjs/jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['/jest.setup.ts'],
+};
