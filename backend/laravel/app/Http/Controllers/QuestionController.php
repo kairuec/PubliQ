69aca34e2aa74f7a48f81dac1e701c9efe53f4e3
kairuec <sakurai1991@gmail.com>
2024-01-25 22:20:03 +0900

    gemini導入

--- backend/laravel/app/Http/Controllers/QuestionController.php
+++ backend/laravel/app/Http/Controllers/QuestionController.php
@@ -11,6 +11,7 @@ use App\Services\Api\chatGptService;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use GeminiAPI\Laravel\Facades\Gemini;
 
 class QuestionController extends Controller
 {
@@ -47,35 +48,38 @@ class QuestionController extends Controller
 
         if ($mode == 'quiz') {
             $prompt = <<<EOD
-            クイズの判定の役割をお願いします。
+            クイズの判定の役割をお願いします
             #お題
             {$genre}
             #正解のワード
             {$answer}
-            #お願いしたい事
-            ユーザーの回答が正解の場合は「correct」とお答えください。
-            違っていた場合は「different」と答えてください。
+            #お願い
+            回答が正解の場合は「正解」
+            違う場合は「違う」と答えて
+            #回答
+            $sentence;
             EOD;
-            $chat_response = chatGptService::store($prompt, "{$sentence}?");
+            $chat_response = Gemini::generateText($prompt);
         }
 
         if ($mode == 'question') {
             $prompt = <<<EOD
-            「yes」か「no」で答えてください。
-            答えられない場合は「unable」と答えてください。
+            質問に「はい」か「いいえ」か「回答できません」と答えて
+            #質問
+            「{$answer}」は{$sentence}?
             EOD;
-            $chat_response = chatGptService::store($prompt, "「{$answer}」は{$sentence}?");
+            $chat_response = Gemini::generateText($prompt);
         }
 
         Log::info($chat_response);
 
-        if (strstr($chat_response, 'correct') !== false) {
+        if (strstr($chat_response, '正解') !== false) {
             $result = "正解！";
-        } else if (strstr($chat_response, 'different') !== false) {
+        } else if (strstr($chat_response, '違う') !== false) {
             $result = "違う！";
-        } else if (strstr($chat_response, 'unable') !== false) {
+        } else if (strstr($chat_response, '回答できません') !== false) {
             $result = "回答できません";
-        } else if (strstr($chat_response, 'yes') !== false) {
+        } else if (strstr($chat_response, 'はい') !== false) {
             $result = "はい";
         } else {
             $result = "いいえ";
