50e902d26f63ce518868f2a3ff6229151c18b72c
kairuec <sakurai1991@gmail.com>
2024-01-25 22:02:58 +0900

    gemini導入

---
+++ backend/laravel/app/Services/Api/geminiService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services\Api;
+
+use GeminiAPI\Laravel\Facades\Gemini;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+
+
+class geminiService
+{
+    public static function store()
+    {
+        $test = Gemini::generateText('「コナン」は工藤優作？「はい」か「いいえ」か「回答できません」と答えて');
+        dd($test);
+
+        // $genre = '食べ物';
+        // $correctAnswer = '寿司';
+        // $userAnswer = 'SUSHI';
+
+        // $prompt = <<<EOD
+        // クイズの判定の役割をお願いします
+        // #お題
+        // {$genre}
+        // #正解のワード
+        // {$correctAnswer}
+        // #お願い
+        // 回答が正解の場合は「正解」
+        // 違う場合は「違う」と答えて
+        // #回答
+        // $userAnswer;
+        // EOD;
+
+        $test = Gemini::generateText($prompt);
+        dd($test);
+    }
+}
